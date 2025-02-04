5b43bba495a348fb9b73e2832f6ea8ad9bd22735
ioannis <agorgianitisj@hotmail.com>
2025-02-04 18:55:14 +0000

    init detector

--- emotion_detection.py
+++ emotion_detection.py
@@ -9,23 +9,9 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
 
     # Set the headers with the required model ID for the API
-    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
     # Make a POST request to the API with the payload and headers
     response = requests.post(url, json=myobj, headers=header)
-
-    # Parse the response from the API
-    formatted_response = json.loads(response.text)
-
-    # If the response status code is 200, extract the label and score from the response
-    if response.status_code == 200:
-        label = formatted_response['documentSentiment']['label']
-        score = formatted_response['documentSentiment']['score']
-
-    # If the response status code is 500, set label and score to None
-    elif response.status_code == 500:
-        label = None
-        score = None
         
-    # Return the label and score in a dictionary
-    return {'label': label, 'score': score}
+    return response.text
